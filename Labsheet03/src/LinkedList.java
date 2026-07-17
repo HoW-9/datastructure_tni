7d2c674b99cab7d785cc90deecb08ad6312dbb3f
Sirapat Tanavitjarearnkul <2513110052@tni.ac.th>
2026-07-17 10:47:18 +0700

    commit

---
+++ Labsheet03/src/LinkedList.java
@@ -0,0 +1,100 @@
+
+public class LinkedList {
+	private Node head; // The first node of the list
+
+	public LinkedList() {
+		this.head = null;
+
+	}
+
+	public String traversal() {
+		Node current_node = head;
+		String result = "[";
+		boolean fisrt_node = true;
+		while (current_node != null) {
+			result += (!fisrt_node ? ", " : "") + current_node.data;
+			current_node = current_node.next;
+			fisrt_node = false;
+		}
+		result += "]";
+		return result;
+	}
+
+	public void insert(int position, Object value) {
+		Node new_Node = new Node(value);
+		if (head == null) {
+			head = new_Node;
+		} else if (position == 0) {
+			new_Node.next = head;
+			head = new_Node;
+		} else {
+			Node current_node = head;
+			int current_position = 0;
+			while (current_node != null && current_position < position - 1) {
+				current_node = current_node.next;
+				current_position++;
+			}
+			new_Node.next = current_node.next;
+			current_node.next = new_Node;
+		}
+	}
+
+	public void insert(Object value) {
+		Node new_Node = new Node(value);
+		if (head == null) {
+			head = new_Node;
+		} else {
+			Node current_node = head;
+			while (current_node.next != null) {
+				current_node = current_node.next;
+			}
+			current_node.next = new_Node;
+		}
+	}
+	public void remove(int position) {
+		if (position == 0) {
+			Node current_node = head;
+			head = current_node.next;
+		}else {
+			Node current_node = head;
+			int current_position = 0;
+			while (current_node.next != null && current_position < position - 1) {
+				current_node = current_node.next;
+				current_position++;
+			}
+			current_node.next = current_node.next.next;
+		}
+	}
+	
+	public void removeLastElement() {
+		if(head != null) {
+			if (head.next == null) {
+				head = null;
+			}else {
+				Node current_node = head;
+				while (current_node.next.next != null) {
+					current_node = current_node.next;
+				}
+				current_node.next = null;
+			}
+		}
+	}
+	public int length() {
+		return 0;
+	}
+	public Object get(int position) {
+		return null;
+	}
+	public void set(int position,Object value) {
+		// update data in node at the position
+	}
+	public void clear() {
+		head = null;
+	}
+	
+	
+	
+	
+	
+	
+}
